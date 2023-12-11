b67a647a6f94e2db78c384b57e624234bb3c6e1a
Bakitten <123baki456@gmail.com>
2023-12-11 18:14:35 +0200

    V4

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Girly Pop Photos',
+      title: 'Unsplash Flutter App with search',
       theme: ThemeData(
-        primaryColor: Colors.pinkAccent,
-        scaffoldBackgroundColor: Colors.yellow[100],
+        scaffoldBackgroundColor: Colors.pink[300],
         appBarTheme: AppBarTheme(
-          color: Colors.pinkAccent,
-        ),
-        textTheme: const TextTheme(
-          headline6: TextStyle(
-            color: Colors.pinkAccent,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-          subtitle1: TextStyle(
-            color: Colors.black,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
+          color: Colors.pink[900],
         ),
       ),
       home: const MyHomePage(),
@@ -62,12 +49,14 @@ class UnsplashApi {
   static const String apiUrl = 'https://api.unsplash.com';
   static const int perPage = 10;
 
+  // Function to search photos using the Unsplash API
   static Future<List<Map<String, dynamic>>> searchPhotos(String query, int page) async {
     final http.Response response = await http.get(
       Uri.parse('$apiUrl/search/photos?query=$query&page=$page&per_page=$perPage'),
       headers: <String, String>{'Authorization': 'Client-ID $apiKey'},
     );
 
+    // Check if the response is OK
     if (response.statusCode == 200) {
       final dynamic data = json.decode(response.body);
       final List<Map<String, dynamic>> results = List<Map<String, dynamic>>.from(data['results'] as List<dynamic>);
@@ -79,17 +68,18 @@ class UnsplashApi {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key});
+  const MyHomePage({super.key});
 
+  // Build the main page, similar to the previous homework( a.k.a. I just copied the code from the previous homework xD )
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Photos',
+          'Unsplash Flutter App with search functionality',
           style: TextStyle(
             color: Colors.white,
-            fontSize: 28,
+            fontSize: 32,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -100,29 +90,33 @@ class MyHomePage extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
+              // Navigate to the search page
               MaterialPageRoute<void>(builder: (BuildContext context) => const SearchPage()),
             );
           },
-          child: const Text('Search Photos', style: TextStyle(fontSize: 20)),
+          child: const Text('Search Photos'),
         ),
       ),
     );
   }
 }
 
+// New class to represent the search page
 class SearchPage extends StatefulWidget {
-  const SearchPage({Key? key});
+  const SearchPage({super.key});
 
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
+// New class to track the state of the search page
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _searchController = TextEditingController();
   final List<Photo> _photos = <Photo>[];
+  // variables to track the page number and loading state
   int _page = 1;
   bool _loading = false;
-  bool _noResults = false;
+  bool _noResults = false; // variable to track if there are no results
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -136,28 +130,34 @@ class _SearchPageState extends State<SearchPage> {
       _loading = true;
     });
 
+    // Call the Unsplash API and wait for the results
     final List<Map<String, dynamic>> photos = await UnsplashApi.searchPhotos(query, _page);
 
     setState(() {
       if (_page == 1) {
         _photos.clear();
-        _noResults = false;
+        _noResults = false; // Reset the noResults flag when starting a new search
       }
 
+      // Add the results to the list
       _photos.addAll(photos.map((Map<String, dynamic> json) => Photo.fromJson(json)));
       _page++;
 
+      // If there are no results, set the noResults flag to true
       if (_photos.isEmpty) {
         _noResults = true;
       }
 
+      // Set the loading flag to false
       _loading = false;
     });
   }
 
+  // Function to listen to the scroll event and load more photos when the user reaches the bottom of the list
   void _scrollListener() {
     if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent &&
         _scrollController.offset > 0) {
+      // Load more photos when the user reaches the bottom of the list
       _loadPhotos(_searchController.text);
     }
   }
@@ -180,8 +180,11 @@ class _SearchPageState extends State<SearchPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
+            // Search bar and button
             TextField(
+              // Set the controller to track the search query
               controller: _searchController,
+              // Set the keyboard type to text
               decoration: const InputDecoration(
                 labelText: 'Search',
                 suffixIcon: Icon(Icons.search),
@@ -202,7 +205,7 @@ class _SearchPageState extends State<SearchPage> {
                   : _photos.isEmpty
                       ? _noResults
                           ? const Center(child: Text('No results found. Try a different search.'))
-                          : Container()
+                          : Container() // Show nothing when the search results are being loaded
                       : _buildPhotoList(),
             ),
           ],
@@ -213,10 +216,11 @@ class _SearchPageState extends State<SearchPage> {
 
   Widget _buildPhotoList() {
     return ListView.builder(
-      itemCount: _photos.length + 1,
+      itemCount: _photos.length + 1, // Add 1 for loading indicator
       itemBuilder: (BuildContext context, int index) {
         if (index < _photos.length) {
           final Photo photo = _photos[index];
+          // Limit the description to 50 characters
           final String limitedDescription =
               photo.description.length > 50 ? '${photo.description.substring(0, 50)}...' : photo.description;
 
@@ -232,7 +236,7 @@ class _SearchPageState extends State<SearchPage> {
                   style: const TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                    color: Colors.white,
                   ),
                 ),
                 contentPadding: const EdgeInsets.all(8.0),
